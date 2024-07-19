545e0dfe19e51cfebaa2976b359daef9899da679
ranielhiyan <ranielhiyan184@gmail.com>
2024-07-19 14:07:02 +0800

    update autoreact.js

--- script/event/autoreact.js
+++ script/event/autoreact.js
@@ -22,12 +22,12 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   if (haha.includes("🥵") || haha.includes("umah") || haha.includes("💋") || haha.includes("kiss") || haha.includes("babu") || haha.includes("wow") || haha.includes("wah") || haha.includes("relationship") || haha.includes("gf") || haha.includes("baby") || haha.includes("omg")){
     return api.setMessageReaction("😘", event.messageID, (err) => {}, true)
         };
-  if (haha.includes("nat") || haha.includes("natsuki")){
+  if (haha.includes("") || haha.includes("")){
     api.setMessageReaction("😡", event.messageID, (err) => {}, true)
-    api.sendMessage("ayaw lagi sigeg pangita ana naa nanay murag uyab.", event.threadID,event.messageID);
+    api.sendMessage("", event.threadID,event.messageID);
   }
   if (haha.includes("Raniel") || haha.includes("raniel")){
-    api.sendMessage("ayaw sigeg pangita ato naa natoy murag uyab.", event.threadID, event.messageID);
+    api.sendMessage("", event.threadID, event.messageID);
   }
   if (haha.includes("What if") || haha.includes("what if")){
     api.sendMessage("what if bobo ka?", event.threadID, event.messageID);
@@ -39,7 +39,7 @@ if (haha.includes("good night") || haha.includes("night")){
     api.sendMessage("good night mahal, sleep well sweet dreams loveu mwa", event.threadID, event.messageID);
 }
 if (haha.includes("piste") || haha.includes("yawa")){
-    api.sendMessage("aysig pamalikas kay maka baho nag nawng", event.threadID, event.messageID);
+    api.sendMessage("", event.threadID, event.messageID);
 };
   
       
