6fddbcfc6109de7c77404761dde02551314020c6
hiyanraniel <hiyanraniel@gmail.com>
2024-08-06 23:40:18 +0800

    Update Aiv2.js

--- script/Aiv2.js
+++ script/Aiv2.js
@@ -6,7 +6,7 @@ module.exports.config = {
   name: 'ai',
   version: '1.0.0',
   role: 0,
-  hasPrefix: true,
+  hasPrefix: false,
   description: "An AI command powered by Hercai",
   usage: "hercai [prompt]",
   credits: 'Developer',
