d37a89378adf7edaab70140a85cfa20df842ce50
simsimdaks@gmail.com <simsimdaks@gmail.com>
2024-08-07 21:02:14 +0800

    update owner.js

--- script/owner.js
+++ script/owner.js
@@ -24,10 +24,9 @@ module.exports.run = async function({ api, event, args, prefix, admin }) {
 		let seconds = Math.floor(time % 60);
 		const uptimeString = `${years > 0 ? `${years} years ` : ''}${months > 0 ? `${months} months ` : ''}${weeks > 0 ? `${weeks} weeks ` : ''}${days % 7 > 0 ? `${days % 7} days ` : ''}${hours > 0 ? `${hours} hours ` : ''}${minutes > 0 ? `${minutes} minutes ` : ''}${seconds} seconds`;
 
-		const CREATORLINK = "https://www.facebook.com/Churchill.Dev4100";
-		const BOTCREATOR = "churchill";
+		const CREATORLINK = "https://www.facebook.com/100092248658233";
+		const BOTCREATOR = "Raniel";
 		const BOTNAME = "𝐀𝐮𝐭𝐨𝐛𝐨𝐭 𝐛𝐲 Raniel";
-		const FILESOWNER = "Nyel";
 		const juswa = moment.tz("Asia/Manila").format("『D/MM/YYYY』 【HH:mm:ss】");
 		const link = ["https://i.imgur.com/9LDVC57.mp4", "https://i.imgur.com/r7IxgiR.mp4", "https://i.imgur.com/J1jWubu.mp4", "https://i.imgur.com/DJylTiy.mp4", "https://i.imgur.com/v4mLGte.mp4", "https://i.imgur.com/uthREbe.mp4", "https://i.imgur.com/ee8fHna.mp4", "https://i.imgur.com/VffzOwS.mp4", "https://i.imgur.com/ci5nztg.mp4", "https://i.imgur.com/qHPeKDV.mp4", "https://i.imgur.com/Rkl5UmH.mp4",
 "https://i.imgur.com/IGXINCB.mp4",
@@ -86,12 +85,9 @@ module.exports.run = async function({ api, event, args, prefix, admin }) {
 
 ⁂ Bot Name: Autobot
 ✧ Bot Admin: Raniel Bigdick
-♛ Bot Admin Link: https://www.facebook.com/RanielHiyanBigdick
+♛ Bot Admin Link: https://www.facebook.com/100092248658233
 ❂ Bot Prefix: ${prefix}
-✫ Files Owner: ${FILESOWNER}
-➟ UPTIME ${uptimeString}
 ✬ Today is: ${juswa} 
-
 ➳ Bot is running ${hours}:${minutes}:${seconds}.
 ✫ Thanks for using my bot`,
 						attachment: fs.createReadStream(__dirname + "/cache/owner_video.mp4")
