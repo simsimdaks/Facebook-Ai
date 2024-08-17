7462844c385bb4a73e9ff64dbf4139c343377494
simsimdaks <154604889+simsimdaks@users.noreply.github.com>
2024-08-17 18:17:04 +0800

    Update Ai.js

--- script/Ai.js
+++ script/Ai.js
@@ -1,29 +1,49 @@
 const axios = require('axios');
 
 module.exports.config = {
-    name: "aiv2",
-    version: 1.0,
-    credits: "kim",
-    description: "AI",
-    hasPrefix: false,
-    usages: "{pn} [prompt]",
-    aliases: [],
-    cooldown: 0,
+    name: "ai",
+    version: "1.0.0",
+    hasPermission: 0,
+    credits: "ai",//api by jerome
+    description: "Gpt architecture",
+    usePrefix: false,
+    commandCategory: "GPT4",
+    cooldowns: 5,
 };
 
 module.exports.run = async function ({ api, event, args }) {
     try {
-        const prompt = args.join(" ");
-        if (!prompt) {
-            await api.sendMessage("Hey I'm your virtual assistant, ask me a question.", event.threadID);
-            return;
+        const { messageID, messageReply } = event;
+        let prompt = args.join(' ');
+
+        if (messageReply) {
+            const repliedMessage = messageReply.body;
+            prompt = `${repliedMessage} ${prompt}`;
         }
 
-        const response = await axios.get(`https://hiro-ai.vercel.app/ai?ask=${encodeURIComponent(prompt)}`);
-        const answer = response.data.response;
+        if (!prompt) {
+            return api.sendMessage('𝗣𝗹𝗲𝗮𝘀𝗲 𝗽𝗿𝗼𝘃𝗶𝗱𝗲 𝗮 𝗽𝗿𝗼𝗺𝗽𝘁 𝘁𝗼 𝗴𝗲𝗻𝗲𝗿𝗮𝘁𝗲 𝗮 𝘁𝗲𝘅𝘁 𝗿𝗲𝘀𝗽𝗼𝗻𝘀𝗲.\n𝗘𝘅𝗮𝗺𝗽𝗹𝗲: 𝗮𝗶 𝘄𝗵𝗮𝘁 𝗶𝘀 𝘄𝗮𝘃𝗲?', event.threadID, messageID);
+        }
+        api.sendMessage('🕧|𝗦𝗲𝗮𝗿𝗰𝗵𝗶𝗻𝗴 𝗳𝗼𝗿 𝗮𝗻 𝗮𝗻𝘀𝘄𝗲𝗿 𝘆𝗼𝘂𝗿 𝗾𝘂𝗲𝘀𝘁𝗶𝗼𝗻 𝗽𝗹𝗲𝗮𝘀𝗲 𝘄𝗮𝗶𝘁...', event.threadID);
 
-        await api.sendMessage(answer, event.threadID);
+        // Delay
+        await new Promise(resolve => setTimeout(resolve, 2000)); // Adjust the delay time as needed
+
+        const gpt4_api = `https://gpt4withcustommodel.onrender.com/gpt?query=${encodeURIComponent(prompt)}&model=gpt-3.5-turbo-16k-0613`;
+
+        const response = await axios.get(gpt4_api);
+
+        if (response.data && response.data.response) {
+            const generatedText = response.data.response;
+
+            // Ai Answer Here
+            api.sendMessage(`𝗚𝗣𝗧 3.5 𝗧𝗨𝗥𝗕𝗢 𝗔𝗦𝗦𝗜𝗦𝗧𝗔𝗡𝗧\n━━━━━━━━━━━━━━━━━━\n${generatedText}\n━━━━━━━━━━━━━━━━━━━━━━━━ \n\n━━━━━━━━━━━━━━━━━━\nThis Ai is made by ICT students in Pau Excellencia Global Academy Foundation , Inc.(Pegafi)\n━━━━━━━━━━━━━━━━━━`, event.threadID, messageID);
+        } else {
+            console.error('API response did not contain expected data:', response.data);
+            api.sendMessage(`❌ An error occurred while generating the text response. Please try again later. Response data: ${JSON.stringify(response.data)}`, event.threadID, messageID);
+        }
     } catch (error) {
-        console.error("Error:", error.message);
+        console.error('Error:', error);
+        api.sendMessage(`❌ An error occurred while generating the text response. Please try again later. Error details: ${error.message}`, event.threadID, event.messageID);
     }
 };
